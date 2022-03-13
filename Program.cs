bb43efccd09b1e94c750f93681a99223a49c7f4e
Carlos Santos <Carlos Santos@LAPTOP-H2K9SUQI>
2022-03-12 22:12:46 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios08
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("recorre los numeros de dos en dos");
+            
+            for (int i= 1; i<=100; i++)
+            {
+                if( (i%2) == 0)
+                {
+                    Console.Write(i + "  ");
+                }
+            }
+                
+
+            Console.ReadLine();
+        }
+    }
+}
